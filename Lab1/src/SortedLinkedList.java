d7bc948fb4dd9c8d481e1542ac77385a166d081a
Dima <dimonty2003@gmail.com>
2021-03-01 20:15:21 +0200

    Added inner class Node of class SortedLinkedList

--- Lab1/src/SortedLinkedList.java
+++ Lab1/src/SortedLinkedList.java
@@ -1,3 +1,37 @@
-public class SortedLinkedList {
-    
+public class SortedLinkedList<T extends Comparable<T>> {
+
+
+    private class Node {
+        private T value;
+        private Node next;
+
+        private Node(T value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        private Node(T value) {
+            this.value = value;
+        }
+
+        private Node(Node next) {
+            this.next = next;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
 }
