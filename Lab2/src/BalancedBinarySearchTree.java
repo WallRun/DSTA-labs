08317958b9111900dd3bc94035cc120df6f77119
WallRun <dimonty2003@gmail.com>
2021-04-02 10:48:07 +0300

    added node class

---
+++ Lab2/src/BalancedBinarySearchTree.java
@@ -0,0 +1,43 @@
+public class BalancedBinarySearchTree {
+    private Node root;
+
+
+
+
+
+    
+
+
+
+
+    private class Node {
+        private Node left;
+        private int value;
+        private Node right;
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+    }
+
+}
